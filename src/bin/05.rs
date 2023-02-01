17ad13e062ed7d6d029fccfa2f668b47be631f17
Camiel Oerlemans <camiel.oerlemans@gmail.com>
2023-02-01 19:58:17 +0100

    day 5 solved

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,9 +1,79 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+use std::collections::VecDeque;
+
+use itertools::enumerate;
+use regex::Regex;
+
+fn parse_crates(input: &str) -> Vec<VecDeque<char>> {
+    let mut crate_lines: Vec<Vec<char>> = vec![];
+    for l in input.lines() {
+        if !l.contains("[") {
+            break;
+        };
+        crate_lines.push(
+            l.char_indices()
+                .filter_map(|(i, c)| if i % 4 == 1 { Some(c) } else { None })
+                .collect(),
+        )
+    }
+    // println!("lines: {:?}", crate_lines);
+
+    let mut crates: Vec<VecDeque<char>> = vec![VecDeque::new(); crate_lines[0].len()];
+    for l in crate_lines {
+        for (i, c) in enumerate(l) {
+            if c != ' ' {
+                crates[i].push_back(c);
+            }
+        }
+    }
+    println!("crates: {:?}", crates);
+
+    return crates;
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<String> {
+    let mut crates = parse_crates(input);
+
+    let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+
+    for a in re.captures_iter(input) {
+        let (nr, from, to) = (
+            a[1].parse::<usize>().unwrap(),
+            a[2].parse::<usize>().unwrap() - 1,
+            a[3].parse::<usize>().unwrap() - 1,
+        );
+
+        for _ in 0..(nr) {
+            let acrate = crates[from].pop_front().unwrap();
+            crates[to].push_front(acrate);
+        }
+    }
+    // println!("{:?}", crates);
+    let answer: Vec<char> = crates.iter().map(|c| c[0]).collect();
+
+    Some(answer.into_iter().collect::<String>())
+}
+
+pub fn part_two(input: &str) -> Option<String> {
+    let mut crates = parse_crates(input);
+
+    let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+
+    for a in re.captures_iter(input) {
+        let (nr, from, to) = (
+            a[1].parse::<usize>().unwrap(),
+            a[2].parse::<usize>().unwrap() - 1,
+            a[3].parse::<usize>().unwrap() - 1,
+        );
+
+        for i in 0..(nr) {
+            let acrate = crates[from].pop_front().unwrap();
+            crates[to].insert(i, acrate);
+        }
+    }
+    // println!("{:?}", crates);
+    let answer: Vec<char> = crates.iter().map(|c| c[0]).collect();
+
+    Some(answer.into_iter().collect::<String>())
 }
 
 fn main() {
@@ -19,12 +89,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some("CMZ".into()));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some("MCD".into()));
     }
 }
