578d2950d9980321d1d1d28085c91d4c75195f00
Camiel Oerlemans <camiel.oerlemans@gmail.com>
2023-02-01 10:58:35 +0100

    solved day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,9 +1,40 @@
+use itertools::Itertools;
+
+fn check_containment(line: &str) -> u32 {
+    let output: Option<bool> = line
+        .split(',')
+        .map(|elf| {
+            elf.split('-')
+                .map(|num| num.parse::<u32>().unwrap_or(0))
+                .collect::<Vec<u32>>()
+        })
+        .tuples()
+        .map(|(e1, e2)| (e1[0] <= e2[0] && e1[1] >= e2[1]) || (e1[0] >= e2[0] && e1[1] <= e2[1]))
+        .reduce(|a, b| a & b);
+
+    output.unwrap_or(false) as u32
+}
+
+fn check_overlap(line: &str) -> u32 {
+    let output: Option<bool> = line
+        .split(',')
+        .map(|elf| {
+            elf.split('-')
+                .map(|num| num.parse::<u32>().unwrap_or(0))
+                .collect::<Vec<u32>>()
+        })
+        .tuples()
+        .map(|(e1, e2)| e1[1] >= e2[0] && e1[0] <=e2[1])
+        .reduce(|a, b| a & b);
+    output.unwrap_or(false) as u32
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    Some(input.lines().map(|l| check_containment(l)).sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(input.lines().map(|l| check_overlap(l)).sum())
 }
 
 fn main() {
@@ -19,12 +50,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(2));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(4));
     }
 }
