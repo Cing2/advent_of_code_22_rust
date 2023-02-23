da06b919a14cddbbbe36c759def516f5bd9d6a3c
Camiel Oerlemans <camiel.oerlemans@gmail.com>
2023-02-24 00:27:40 +0100

    revert to working solution for 1

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -2,14 +2,20 @@ use std::collections::HashMap;
 
 use regex::Regex;
 
+type ValveKey = [char; 2];
+
 #[derive(Debug)]
-struct Valve<'a> {
+struct Valve {
     flow_rate: i32,
-    leads_to: Vec<&'a str>,
+    leads_to: Vec<ValveKey>,
 }
 
-fn parse_input(input: &str) -> HashMap<&str, Valve> {
-    let mut valves = HashMap::new();
+fn valvekey_from_str(input: &str) -> ValveKey {
+    [input.chars().nth(0).unwrap(), input.chars().nth(1).unwrap()] as ValveKey
+}
+
+fn parse_input(input: &str) -> HashMap<ValveKey, Valve> {
+    let mut valves: HashMap<ValveKey, Valve> = HashMap::new();
 
     let re = Regex::new(r"Valve (?P<name>\w+) has flow rate=(?P<rate>\d+); tunnels* leads* to valves* (?P<valves>.*)").unwrap();
 
@@ -20,13 +26,14 @@ fn parse_input(input: &str) -> HashMap<&str, Valve> {
             .as_str()
             .split(",")
             .map(str::trim)
+            .map(valvekey_from_str)
             .collect();
         let new_valve = Valve {
             flow_rate: cap.name("rate").unwrap().as_str().parse::<i32>().unwrap(),
             leads_to: to_valves,
         };
         let name = cap.name("name").unwrap().as_str();
-        valves.insert(name, new_valve);
+        valves.insert(valvekey_from_str(name), new_valve);
     }
 
     // dbg!(&valves);
@@ -34,22 +41,22 @@ fn parse_input(input: &str) -> HashMap<&str, Valve> {
 }
 
 fn simulate_valves(
-    valves: &HashMap<&str, Valve>,
+    valves: &HashMap<ValveKey, Valve>,
     max_open_valves: usize,
     min_left: i32,
-    at_valve_name: &str,
-    open_valves: &Vec<&str>,
-    visited_valves_since_open: &Vec<&str>,
+    at_valve_name: ValveKey,
+    open_valves: &mut Vec<ValveKey>,
+    visited_valves_since_open: &Vec<ValveKey>,
 ) -> i32 {
     // do not allow to visited more then 5 files without opening
-    if visited_valves_since_open.len() > 4 {
+    if visited_valves_since_open.len() > 5 {
         return 0;
     }
     if min_left == 0 {
         return 0;
     }
 
-    let current_valve = valves.get(at_valve_name).unwrap();
+    let current_valve = valves.get(&at_valve_name).unwrap();
     // update the current pressure
     let current_pressure: i32 = open_valves
         .iter()
@@ -65,20 +72,20 @@ fn simulate_valves(
     let mut max_buildup_pressure = 0;
 
     // open current valve if possible
-    let opening_pressure = if !open_valves.contains(&at_valve_name) && current_valve.flow_rate > 0 {
-        let mut new_open_valves = open_valves.clone();
-        new_open_valves.push(at_valve_name);
-        simulate_valves(
+    let mut opening_pressure = 0;
+    if !open_valves.contains(&at_valve_name) && current_valve.flow_rate > 0 {
+        open_valves.push(at_valve_name);
+        opening_pressure = simulate_valves(
             valves,
             max_open_valves,
             min_left - 1,
             at_valve_name,
-            &new_open_valves,
+            open_valves,
             &vec![at_valve_name],
-        )
-    } else {
-        0
-    };
+        );
+        open_valves.pop();
+        
+    }
     max_buildup_pressure = max_buildup_pressure.max(opening_pressure);
 
     // move to new valves
@@ -91,8 +98,8 @@ fn simulate_valves(
                 valves,
                 max_open_valves,
                 min_left - 1,
-                next_valve,
-                &open_valves,
+                *next_valve,
+                open_valves,
                 &new_visited_valves,
             ));
         }
@@ -105,113 +112,22 @@ pub fn part_one(input: &str) -> Option<i32> {
     let valves = parse_input(input);
 
     let nr_valves_with_pressure: usize = valves.iter().filter(|(_, v)| v.flow_rate > 0).count();
-
-    let workers = vec![ValveWorker{ at_valve: "JJ", visited_valves_since_open: vec![] }];
-
-
-    Some(simulate_valves_elephant(
+    let mut open_valves = vec![];
+    Some(simulate_valves(
         &valves,
         nr_valves_with_pressure,
         30,
+        valvekey_from_str("AA"),
+        &mut open_valves,
         &vec![],
-        workers
     ))
-}
-
-#[derive(Debug, Clone)]
-struct ValveWorker<'a> {
-    at_valve: &'a str,
-    visited_valves_since_open: Vec<&'a str>,
-}
-
-
-fn simulate_valves_elephant(
-    valves: &HashMap<&str, Valve>,
-    max_open_valves: usize,
-    min_left: i32,
-    open_valves: &Vec<&str>,
-    workers: Vec<ValveWorker>,
-) -> i32 {
-    if min_left == 0 {
-        return 0;
-    }
-    // building pressure on this minute
-    let current_pressure: i32 = open_valves
-        .iter()
-        .map(|v| valves.get(v).unwrap().flow_rate)
-        .sum();
-
-    // check if we have all valves open
-    if open_valves.len() == max_open_valves {
-        return current_pressure * min_left;
-    }
-
-    // for each possible value return the maximum
-    let mut max_buildup_pressure = 0;
-
-    for i in 0..workers.len() {
-        let worker = &workers[i];
-        // do not allow to visited more then 5 files without opening
-        if worker.visited_valves_since_open.len() > 4 {
-            return 0;
-        }
-
-        let current_valve = valves.get(worker.at_valve).unwrap();
-
-        // open current valve if possible
-        let opening_pressure =
-            if !open_valves.contains(&worker.at_valve) && current_valve.flow_rate > 0 {
-                let mut new_open_valves = open_valves.clone();
-                new_open_valves.push(worker.at_valve);
-                simulate_valves_elephant(
-                    valves,
-                    max_open_valves,
-                    min_left - 1,
-                    &new_open_valves,
-                    workers.clone(),
-                )
-            } else {
-                0
-            };
-        max_buildup_pressure = max_buildup_pressure.max(opening_pressure);
-
-        // move to new valves
-        for next_valve in &current_valve.leads_to {
-            // do not allow to loop back to valves if not opening one
-            if !worker.visited_valves_since_open.contains(next_valve) {
-                let mut new_workers = workers.clone();
-                new_workers[i].visited_valves_since_open.push(&next_valve);
-                new_workers[i].at_valve = next_valve;
-
-                max_buildup_pressure = max_buildup_pressure.max(simulate_valves_elephant(
-                    valves,
-                    max_open_valves,
-                    min_left - 1,
-                    &open_valves,
-                    new_workers,
-                ));
-            }
-        }
-    }
-
-    current_pressure + max_buildup_pressure
+    // None
 }
 
 pub fn part_two(input: &str) -> Option<i32> {
     let valves = parse_input(input);
 
-    let nr_valves_with_pressure: usize = valves.iter().filter(|(_, v)| v.flow_rate > 0).count();
-
-    let workers = vec![ValveWorker{ at_valve: "JJ", visited_valves_since_open: vec![] }];
-
-
-    Some(simulate_valves_elephant(
-        &valves,
-        nr_valves_with_pressure,
-        30,
-        &vec![],
-        workers
-    ))
+    None
 }
 
 fn main() {
@@ -233,6 +149,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 16);
-        assert_eq!(part_two(&input), Some(1707));
+        assert_eq!(part_two(&input), None);
     }
 }
